758e15479d30161ac5e4604f476127cf39f99c3b
eladanius <temiron-94@mail.ru>
2022-03-29 23:54:03 +0300

    add schedule, add countdown timer

--- src/components/task/index.tsx
+++ src/components/task/index.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import SchedulePopup from './subcomponents/SchedulePopup';
 import './taskComponent.css';
-
+import calculateTimeLeft from './utils/countdownTimer';
+import { Stopwatch } from 'react-bootstrap-icons';
 interface Props {
   title: string;
   content: string;
@@ -9,9 +10,31 @@ interface Props {
 
 const TaskComponent = ({ title, content }: Props) => {
   const [schedulePopupVisible, setSchedulePopupVisible] = useState(false);
+  const [scheduledDate, setScheduledDate] = useState<string>();
+  const [scheduledTime, setScheduledTime] = useState<string>();
+  const [timeLeft, setTimeLeft] = useState<any>();
+
+  useEffect(() => {
+    if (scheduledDate) {
+      const timer = setTimeout(() => {
+        setTimeLeft(calculateTimeLeft(scheduledDate || '', scheduledTime || ''));
+      }, 1000);
+
+      return () => clearTimeout(timer);
+    }
+  });
+  console.log(timeLeft);
+
   return (
     <>
-      <SchedulePopup isVisible={schedulePopupVisible} setVisible={setSchedulePopupVisible} />
+      <SchedulePopup
+        isVisible={schedulePopupVisible}
+        setVisible={setSchedulePopupVisible}
+        date={scheduledDate}
+        time={scheduledTime}
+        setDate={setScheduledDate}
+        setTime={setScheduledTime}
+      />
       {schedulePopupVisible ? <div className='screenlocker'></div> : <></>}
       <div className='task_row'>
         <div className='task_title'>
@@ -19,11 +42,34 @@ const TaskComponent = ({ title, content }: Props) => {
         </div>
         <div className='task_body'>
           <span>{content}</span>
+          <div className='schedule_settings'>
+            <h5>Date: </h5>
+            <span className='schedule_settings__element'>{scheduledDate || '??-??-????'}</span>
+            <h5>Time: </h5>
+            <span className='schedule_settings__element'>{scheduledTime || '??-??'}</span>
+          </div>
         </div>
         <div className='task_actions'>
           <button onClick={() => setSchedulePopupVisible(true)}>Schedule</button>
         </div>
       </div>
+      {!scheduledDate ? (
+        <></>
+      ) : (
+        <div className='timer'>
+          {/* <Stopwatch className='timer-icon' /> */}
+          {Object.keys(timeLeft || {})?.map((key, i) => {
+            return timeLeft[key] === 0 ? (
+              <></>
+            ) : (
+              <>
+                <h5 style={{margin:'0 4px 0 10px'}}>{key}:</h5>
+                <span>{timeLeft[key]}</span>
+              </>
+            );
+          })}
+        </div>
+      )}
     </>
   );
 };
--- src/components/task/subcomponents/SchedulePopup.tsx
+++ src/components/task/subcomponents/SchedulePopup.tsx
@@ -1,19 +1,60 @@
+import { useEffect, useState } from 'react';
 import './schedulePopup.css';
 
 interface Props {
   isVisible: boolean;
   setVisible: (value: boolean) => void;
+  date: string | undefined;
+  time: string | undefined;
+  setTime: (value: string) => void;
+  setDate: (value: string) => void;
 }
 
-const SchedulePopup = ({ isVisible, setVisible }: Props) => {
+const SchedulePopup = ({ isVisible, setVisible, date, time, setDate, setTime }: Props) => {
+
+  const [selectedDate, setSelectedDate] = useState<string>('');
+  const [selectedTime, setSelectedTime] = useState<string>('');
+
+  useEffect(() => {
+    setSelectedDate(date || '');
+    setSelectedTime(time || '');
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isVisible]);
+
+  const handleOk = () => {
+    if (!selectedDate) {
+      alert('Please set data');
+      return;
+    }
+    if (!selectedTime) {
+      alert('Please set time');
+      return;
+    }
+    setDate(selectedDate!);
+    setTime(selectedTime!);
+  };
+  
   return (
     <div className={isVisible ? 'schedule-popup_root' : 'schedule-popup_root hidden'}>
       <div className='schedule-popup_header'>
         <h3>Schedule time:</h3>
       </div>
       <div className='schedule-popup_body'>
+        <div className='row'>
+          <span>Date:</span>
+          <input type='date' value={selectedDate} onChange={(event) => setSelectedDate(event.target.value)} />
+        </div>
+        <div className='row'>
+          <span>Time:</span>
+          <input type='time' defaultValue={selectedTime} onChange={(event) => setSelectedTime(event.target.value)} />
+        </div>
         <div className='schedule-popup_footer'>
-          <button onClick={() => setVisible(false)}>Close</button>
+          <button className='footer_el' onClick={handleOk}>
+            Ok
+          </button>
+          <button className='footer_el' onClick={() => setVisible(false)}>
+            Close
+          </button>
         </div>
       </div>
     </div>
